5ceb7ebf3d956612b3beeb85f803f44a28154cdd
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-03 16:16:36 -0300

    Cria as rotas para a aplicação (Req.01)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ProfileEdit from './pages/ProfileEdit';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes 🎧</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
