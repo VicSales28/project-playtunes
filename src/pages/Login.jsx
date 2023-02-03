9fdc7a27b9ef009a324960cd4f67fdcf5ddfd5ca
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-03 20:02:36 -0300

    Crie um formulário de identificação (Req. 02)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
+  state = {
+    name: '',
+    loading: false,
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState(() => ({
+      [name]: value,
+    }));
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history: { push } } = this.props;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      loading: false,
+    }, () => push('/search'));
+  };
+
   render() {
+    const { name, loading } = this.state;
+    const minLength = 3;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+
+        {loading === true
+          ? <Loading />
+          : (
+            <form>
+              <label htmlFor="name">
+                Nome:
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  onChange={ this.handleChange }
+                  value={ name }
+                  data-testid="login-name-input"
+                />
+              </label>
+
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ name.length < minLength }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+
+            </form>)}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
