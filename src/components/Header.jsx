52dced7adb8b08919b605f3602fd76294df68adc
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-03 21:15:44 -0300

    Cria links de navegação no header (Req.04)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -28,6 +29,13 @@ class Header extends Component {
         {loading === true
           ? <Loading />
           : <p data-testid="header-user-name">{userName}</p>}
+        <div>
+          <nav>
+            <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+            <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+            <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+          </nav>
+        </div>
       </header>
     );
   }
