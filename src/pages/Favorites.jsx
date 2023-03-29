d75dc59e713211c5659b278ec1ebb37d7a5eeac8
Victoria Sales <contatovictoriasales@gmail.com>
2023-03-29 16:18:33 -0300

    Lista favoritas em Favorites page(Req.12)

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import '../styles/pages/Favorites.css';
 
 class Favorites extends Component {
+  state = {
+    favoriteSongs: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  getFavorites = async () => {
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({ favoriteSongs, loading: false });
+  };
+
+  toRemoveSong() {
+    this.setState({ loading: true }, this.getFavorites());
+  }
+
   render() {
+    const { favoriteSongs, loading } = this.state;
+
     return (
-      <div data-testid="page-favorites">
+      <div
+        className="page_favorites"
+        data-testid="page-favorites"
+      >
         <Header />
-        <p>Favorites</p>
+
+        { loading === true
+          ? <Loading />
+          : (
+            <div className="favorites_songs_container">
+
+              {favoriteSongs.map((track) => (
+                <MusicCard
+                  key={ track.trackId }
+                  track={ track }
+                  status={ this.toRemoveSong }
+                />
+              ))}
+
+            </div>
+          )}
+
       </div>
     );
   }
