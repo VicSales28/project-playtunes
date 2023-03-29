72c82ba74c2ee94847f6fa6d435ba8f0bb2918dc
Victoria Sales <contatovictoriasales@gmail.com>
2023-03-29 07:21:26 -0300

    Importa css em NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
+import '../styles/pages/NotFound.css';
 
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        <h1>Ops, 404. Not Found</h1>
+      <div
+        className="page_not_found"
+        data-testid="page-not-found"
+      >
+
+        <Header />
+
+        <div className="page_instructions">
+
+          <h1>Ops, 404. Not Found</h1>
+
+          <Link to="/">
+            <button
+              type="button"
+              className="not_found_button"
+            >
+              Retornar
+            </button>
+          </Link>
+
+        </div>
       </div>
     );
   }
